50a872f05162d0b08304660df1b3df77d1bb20c0
ma'sirfani <muhammadwalle123@gmail.com>
2023-11-07 18:31:15 +0700

    add base template sidebar, navbar, sidenav

---
+++ resources/views/templates/backend/partial-footer.blade.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="container">
+        <div class="footer clearfix mb-0 text-muted">
+            <div class="float-start">
+                <p>{{ date("Y") }} &copy; <a href="https://saugi.me">Mazer</a></p>
+            </div>
+            <div class="float-end">
+                <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by Irfan - <a href="https://pandoradev.id/">PANDORADEV</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Back\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sidebar', function () {
+    return view('templates.backend.main-sidebar');
+});
+Route::get('/navbar', function () {
+    return view('templates.backend.main-top-navbar');
+});
+Route::get('/sidenav', function () {
+    return view('templates.backend.main-sidebar-navbar');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
